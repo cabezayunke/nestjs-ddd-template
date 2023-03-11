ef8df2ec92d64759fe4fd76f6b26e1892583550f
Joserra <jrblancorey@gmail.com>
2023-03-11 13:37:50 +0100

    chore: clean up share domain

--- libs/shared/src/domain/errors/QueryNotRegisteredError.ts
+++
@@ -1,8 +0,0 @@
-import { Query } from '../queries/Query';
-import { DomainError } from './DomainError';
-
-export class QueryNotRegisteredError extends DomainError {
-  constructor(query: Query) {
-    super(`The query <${query.constructor.name}> hasn't a query handler associated`);
-  }
-}
--- libs/shared/src/domain/queries/QueryBus.ts
+++
@@ -1,6 +0,0 @@
-import { Response } from '../Response';
-import { Query } from './Query';
-
-export interface QueryBus {
-  ask<R extends Response>(query: Query): Promise<R>;
-}
